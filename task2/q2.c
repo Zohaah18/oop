ecc811997e2926f8b4e336fd7afe317c9664d6e5
Zohaah18 <142868228+Zohaah18@users.noreply.github.com>
2024-02-09 10:32:08 +0500

    Update q2.c

--- task2/q2.c
+++ task2/q2.c
@@ -1,32 +1,81 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+ 
+ using namespace std;
+class WaterBottle {
+private:
+    string company;
+    string color;
+    int capacity_liters;
+    int capacity_milliliters;
 
-// swap function using XOR 
-void swap(void *a, void *b, size_t size) {
-     char *byte_a = (char *)a;
-      char *byte_b = (char *)b;
-
-    for (size_t i = 0; i < size; ++i) {
-        byte_a[i] ^= byte_b[i];
-        byte_b[i] ^= byte_a[i];
-        byte_a[i] ^= byte_b[i];
+public:
+    WaterBottle(string company, string color, int capacity_liters, int capacity_milliliters) {
+        this->company = company;
+        this->color = color;
+        this->capacity_liters = capacity_liters;
+        this->capacity_milliliters = capacity_milliliters;
     }
-}
+
+    // Getter methods
+    string getCompany() {
+        return company;
+    }
+
+    string getColor() {
+        return color;
+    }
+
+    int getCapacityLiters() {
+        return capacity_liters;
+    }
+
+    int getCapacityMilliliters(){
+        return capacity_milliliters;
+    }
+
+    // Setter methods
+    void setCompany(string company) {
+        this->company = company;
+    }
+
+    void setColor(string color) {
+        this->color = color;
+    }
+
+    void consumeWater(int milliliters) {
+        int consumedliters = milliliters / 1000;
+        int consumedremainder_ml = milliliters % 1000;
+        
+        capacity_liters -= consumed_liters;
+        capacity_milliliters -= consumed_remainder_ml;
+
+        if (capacity_milliliters < 0) {
+            capacity_liters -= 1;
+            capacity_milliliters += 1000;
+        }
+
+        cout << "Water consumed. Updated capacity: " << capacity_liters << " liters, "
+                  << capacity_milliliters << " milliliters." << endl;
+    }
+};
 
 int main() {
-    // Example with integers
-    int x = 5, y = 10;
+    WaterBottle bottle;
 
-    printf("Before swapping: x = %d, y = %d\n", x, y);
-    swap(&x, &y, sizeof(int));
-    printf("After swapping: x = %d, y = %d\n", x, y);
+   
+    cout << "Company: " << bottle.getCompany() <<endl;
+    cout << "Color: " << bottle.getColor() << endl;
+    cout << "Capacity: " << bottle.getCapacityLiters() << " liters, "
+              << bottle.getCapacityMilliliters() << " milliliters" << endl;
 
-    // Example with floats
-    float a = 3.14, b = 2.718;
+    int consumed_milliliters;
+    cout << "Enter the amount of water consumed (in milliliters): ";
+    cin >> consumed_milliliters;
 
-    printf("\nBefore swapping: a = %.2f, b = %.2f\n", a, b);
-    swap(&a, &b, sizeof(float));
-    printf("After swapping: a = %.2f, b = %.2f\n", a, b);
+    bottle.consumeWater(consumed_milliliters);
+
     return 0;
 }
 
-
+  
